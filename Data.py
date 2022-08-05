d4362762e69e49c019822b9d9c68ff4eaa3b6c94
ANES <anessalih34@gmail.com>
2022-08-06 01:12:52 +0300

    Update Data.py

--- Data.py
+++ Data.py
@@ -4,56 +4,54 @@ from pyrogram.types import InlineKeyboardButton
 class Data:
     # Start Message
     START = """
-Hey {}.
-Welcome to {}
+مرحبا {}.
+اهلا بك في {}
 
-I am the Master of Whisperers (like Varys in Game of Thrones).
-يمكنك استخدامني لإرسال رسائل تهمس إلى صديقك في مجموعات وقنوات (حتى لو لم أكن موجودًا).
+انا بوت همسه).
+يمكنك استخدامي لإرسال رسائل تهمسه إلى صديقك في مجموعات وقنوات (حتى لو لم أكن موجودًا).
 فقط هذا الصديق وأنت ستكون قادرًا على قراءة الرسالة على الرغم من وجود آخرين في نفس المجموعة.
 
 لمعرفة كيفية استخدامي ، اضغط على "كيفية الاستخدام" أدناه.
 
-By @S8Y8Y
+By @N_B_1N
     """
 
     # Home Button
     home_buttons = [
-        [InlineKeyboardButton("🔒 أرسل الهمس 🔒", switch_inline_query="")],
-        [InlineKeyboardButton(text="🏠 العودة للمنزل 🏠", callback_data="home")],
+        [InlineKeyboardButton(" أرسل الهمسه ", switch_inline_query="")],
+        [InlineKeyboardButton(text=" العودة للمنزل ", callback_data="home")],
     ]
     # Rest Buttons
     buttons = [
         [
-            InlineKeyboardButton("🔒 ارسل الهمس 🔒", switch_inline_query="")
+            InlineKeyboardButton(" ارسل الهمسه ", switch_inline_query="")
         ],
         [
             InlineKeyboardButton("كيف تستعمل ❔", callback_data="help"),
-            InlineKeyboardButton("🎪 عن 🎪", callback_data="about")
+            InlineKeyboardButton(" عن ", callback_data="about")
         ],
-        [InlineKeyboardButton("♥ المزيد من البوتات المذهلة ♥", url="https://t.me/S8Y8S")],
-        [InlineKeyboardButton("🎨 للابلاغ عن مشكله 🎨", url="https://t.me/N_B_1N")],
+        [InlineKeyboardButton("♥ المزيد من البوتات ♥", url="https://t.me/S8Y8S")],
+        [InlineKeyboardButton(" للابلاغ عن مشكله ", url="https://t.me/N_B_1N")],
     ]
 
     # Help Message
     HELP = """
 ما عليك سوى كتابة الرسالة بالتنسيق أدناه في أي دردشة.
 
-`@WhisperStarkBot your_message friend_username/id`
+`@HMA2Bot رسالتك USER/ID صديقك`
     """
 
     # About Message
     ABOUT = """
 **عن هذا البوت** 
 
-Bot created by @S8Y8S
+مطور البوت [**ANES**](@N_B_1N)
 
-Source Code : [Click Here](https://github.com/ANES0H/WhisperBot)
+قناه البوت [S8Y8S](@S8Y8S) 
 
-Inspired By : nnbbot
+Inspired By : HMA2BOT
 
 Framework : [Pyrogram](docs.pyrogram.org)
 
 Language : [Python](www.python.org)
-
-Developer : @S8Y8S
     """
